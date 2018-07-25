d59218dd5f1391bbcfc200a2b081903c8e73c5da
Jason Dela Torre <jasondelatorre92@gmail.com>
2018-07-25 21:40:54 +0800

    Update: finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const dayList = ['First Day','Second Day','Third Day','Fourth Day','Fifth Day','Sixth Day','Seventh Day','Eighth Day','Ninth Day','Tenth Day','Eleventh Day','Twelfth Day']
-const giftsList = ['Partridge in a pear tree','Two Turtle Doves','Three french hens','Four Calling Birds','Five Golden Rings','Six geese a laying','Seven swams a singing','Eight Maids a Milking','Nine Ladies Dancing','Ten Lords a Leaping','Eleven Pipers Piping','Twelve Drummers Drumming']
+const giftsList = ['a Partridge in a pear tree','Two Turtle Doves','Three french hens','Four Calling Birds','Five Golden Rings','Six geese a laying','Seven swams a singing','Eight Maids a Milking','Nine Ladies Dancing','Ten Lords a Leaping','Eleven Pipers Piping','Twelve Drummers Drumming']
 
 let day = 1;
-let dayMessage;
+let memory = giftsList[0];
 
 function nextDay() {
 	let dayMessageContainer = $('#dayMessageContainer')
@@ -10,20 +10,18 @@ function nextDay() {
 
 	if (day < 12) {
 
-		let newDay = document.createElement("p")
-		let newGiftDay = document.createElement("p") 
-		let newDayMessage = document.createTextNode("ON THE "+ dayList[day] + " OF CHRISTMAS MY TRUE LOVE GAVE TO ME")
-		let newGiftsMessage = document.createTextNode(giftsList[day])
-		newDay.appendChild(newDayMessage)
-		newGiftDay.appendChild(newGiftsMessage)
+		// if (day === 1) memory =  giftsList[day] + ", " + memory
+		memory =  giftsList[day] + ", and " + memory
 
+		console.log(day)
+		
+		let newDay = document.createElement("p")
+		let newDayMessage = document.createTextNode("ON THE " + dayList[day] + " OF CHRISTMAS MY TRUE LOVE SENT TO ME " + memory)
+
+		newDay.appendChild(newDayMessage)
 		let container = document.getElementById('container')
 		container.appendChild(newDay);
-		container.appendChild(newGiftDay);
-		// console.log(dayList[day]);
-		// console.log(giftsList[day]);
-		// dayMessageContainer.text(dayList[day])
-		// giftMessageContainer.text(giftsList[day])
+
 		day++;
 	}
 	else {
